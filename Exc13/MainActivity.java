534294955cc631ed38ef5d78caa640e45d68ad86
anandhu <74704840+Anandhu015@users.noreply.github.com>
2022-02-09 14:22:30 +0530

    Add files via upload

---
+++ Exc13/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.excercise10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1=findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
