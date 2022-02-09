b8279c178c946656b1e3e4bdaaac1e4b7d96c4f8
anandhu <74704840+Anandhu015@users.noreply.github.com>
2022-02-09 15:34:10 +0530

    Add files via upload

---
+++ exc10.java
@@ -0,0 +1,54 @@
+package com.example.excercise_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkBox1:
+                if (checked) {
+                    displaymessage("selected BCA");
+                }
+                else {
+                    displaymessage("removed BCA");
+                }
+                break;
+            case R.id.checkBox2:
+                if (checked) {
+                    displaymessage("selected MCA");
+                }
+                else {
+                    displaymessage("removed MCA");
+                }
+
+                break;
+
+        }
+
+
+
+    }
+    public void displaymessage (String s)
+    {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
+
+
+
